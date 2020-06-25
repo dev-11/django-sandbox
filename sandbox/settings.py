478e1a7a0edb96d3a7d819801fe5cb1ba4431f0e
Otto <otto@masterbranch.io>
2020-06-25 17:05:05 +0100

    readme update

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qwfcm_7&@wq#g8yblwhz3%vl5y_nt4n-^p04ipjb5=d!eo7qfw'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
